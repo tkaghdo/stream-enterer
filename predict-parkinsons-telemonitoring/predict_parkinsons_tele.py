c4e39a3d1d48f1bb04ae98c707c6a399188c7843
Tamby Kaghdo <tamby.kaghdo@gmail.com>
2016-11-03 16:27:03 -0400

    age freq plot

--- predict-parkinsons-telemonitoring/predict_parkinsons_tele.py
+++ predict-parkinsons-telemonitoring/predict_parkinsons_tele.py
@@ -6,6 +6,7 @@ the purpose of this program is to predict the values of motor_UPDRS and total_UP
 
 import pandas as pd
 import sys
+import matplotlib.pyplot as plt
 
 # *** Begin Functions ***
 def remove_nan(df):
@@ -31,7 +32,14 @@ def main():
     print(pd.pivot_table(study_df,index=["sex"]))
 
     #generate frequencies plot by age and sex
-    
+    fig = plt.figure(figsize=(16,8))
+    fig.suptitle("Age Frequencies", fontsize=14)
+    ax1 = fig.add_subplot(1,2,1)
+    ax1.set_xlim([min(study_df["age"]),max(study_df["age"])])
+    ax1.set_xlabel("Age")
+    ax1 = study_df["age"].hist(color="cornflowerblue")
+
+    plt.show()
 
 if __name__ == "__main__":
     sys.exit(0 if main() else 1)
