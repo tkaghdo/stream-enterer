3a69d256e407d41c6f61e7415bf3a416c6279971
Tamby Kaghdo <tamby.kaghdo@gmail.com>
2016-11-11 09:41:23 -0500

    command line

--- wisconsin-prognostic-breast-cancer/predict_wpbc.py
+++ wisconsin-prognostic-breast-cancer/predict_wpbc.py
@@ -98,6 +98,23 @@ def create_train_test(df):
     test_df = shuffled_df[train_length:len(shuffled_df)]
     return train_df, test_df
 
+def prepare_df(file):
+    try:
+        wpbc_df = pd.read_csv(file, sep=",")
+    except:
+        print("Error opening file")
+        sys.exit(1)
+    print("there are {0} rows in the data set".format(len(wpbc_df)))
+    #clean up: some rows have '?' in lymph_node_status. Lets see how many rows are there with '?'
+    temp_df = wpbc_df[wpbc_df["lymph_node_status"] == '?']
+    print("there are {0} rows with '?' in lymph_node_status column".format(len(temp_df)))
+    #there were only 4 rows where lymph_node_status = '?'. So I will delete these 4 rows
+    wpbc_df = wpbc_df[wpbc_df["lymph_node_status"] != '?']
+    print("there are now {0} rows in the data set".format(len(wpbc_df)))
+    #create an integer equivalent of 'outcome'
+    wpbc_df["outcome_integer"] = [0 if x == "N" else 1 for x in wpbc_df["outcome"]]
+    return wpbc_df
+
 # *** END FUNCTIONS
 
 def main():
@@ -111,37 +128,23 @@ def main():
         parser.error("incorrect number of arguments")
         status = False
     else:
-        print validate_pin(sys.argv[1])
+        features = ['recurrence_time', 'cell_radius','cell_texture','cell_perimeter','cell_area','cell_smoothness','cell_compactness', \
+                    'cell_concave_points','cell_symmetry','cell_fractal_dimension','cell_11','cell_12','cell_13','cell_14','cell_15', \
+                    'cell_16','cell_17','cell_18','cell_19','cell_20','cell_21','cell_22','cell_23','cell_24','cell_25','cell_26','cell_27', \
+                    'cell_28','cell_29','cell_30_cell_31','cell_32','tumor_size','lymph_node_status']
+
+        if sys.argv[1].upper() == "ALL":
+            wpbc_df = prepare_df("data/wpbc.data")
+            model_on_whole_data_set(wpbc_df, features)
+        elif sys.argv[1].upper() == "HOLDOUT":
+            wpbc_df = prepare_df("data/wpbc.data")
+            model_using_holdout_validation(wpbc_df, features)
+        else:
+            parser.error("incorrect option")
+            status = False
     return status
-    
-    try:
-        wpbc_df = pd.read_csv("data/wpbc.data", sep=",")
-    except:
-        print("Error opening file")
-        sys.exit(1)
-
-    print("there are {0} rows in the data set".format(len(wpbc_df)))
-    #clean up: some rows have '?' in lymph_node_status. Lets see how many rows are there with '?'
-    temp_df = wpbc_df[wpbc_df["lymph_node_status"] == '?']
-    print("there are {0} rows with '?' in lymph_node_status column".format(len(temp_df)))
-
-    #there were only 4 rows where lymph_node_status = '?'. So I will delete these 4 rows
-    wpbc_df = wpbc_df[wpbc_df["lymph_node_status"] != '?']
-    print("there are now {0} rows in the data set".format(len(wpbc_df)))
-
-    features = ['recurrence_time', 'cell_radius','cell_texture','cell_perimeter','cell_area','cell_smoothness','cell_compactness', \
-                'cell_concave_points','cell_symmetry','cell_fractal_dimension','cell_11','cell_12','cell_13','cell_14','cell_15', \
-                'cell_16','cell_17','cell_18','cell_19','cell_20','cell_21','cell_22','cell_23','cell_24','cell_25','cell_26','cell_27', \
-                'cell_28','cell_29','cell_30_cell_31','cell_32','tumor_size','lymph_node_status']
-
-    #create an integer equivalent of 'outcome'
-    wpbc_df["outcome_integer"] = [0 if x == "N" else 1 for x in wpbc_df["outcome"]]
-
-    model_on_whole_data_set(wpbc_df, features)
-    #model_using_holdout_validation(wpbc_df, features)
 
 if __name__ == "__main__":
     sys.exit(0 if main() else 1)
-    #TODO: modify to run from command line to either create a model on the whole set or do cross validation
+
     #TODO: ROC Curve
-    #TODO: put usage
